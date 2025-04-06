1b5eb2b58033f87df88188556d09390f48f05cd1
Shaikat0079 <baruashaikat00790@gmail.com>
2025-04-07 04:44:20 +0930

    Loaded data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
@@ -6,6 +7,15 @@ function App() {
   return (
     <>
       <Navbar></Navbar>
+      <div className="main-container flex text-center">
+        <div className="left-container w-[70%]">
+          <Blogs></Blogs>
+        </div>
+        <div className="right-container w-[30%]">
+          <h1>Reading Time: 0</h1>
+          <h1>BookedMark Count: 0</h1>
+        </div>
+      </div>
     </>
   )
 }
---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react'
+
+const Blogs = () => {
+    const [blogs,setBlogs] = useState([])
+
+    useEffect(()=>{
+        fetch("blogs.json")
+        .then(res=>res.json())
+        .then(data=>setBlogs(data))
+    },[])
+    return ( 
+        <div>
+            <h1 className='text-4xl'>Available Blogs: {blogs.length}</h1>
+        </div>
+     );
+}
+ 
+export default Blogs;
